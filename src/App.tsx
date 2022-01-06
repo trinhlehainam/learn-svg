737a5be77ae805199f4f5db57cb9ae0e7ce5cc2d
checker <trinhlehainam@protonmail.com>
2022-01-06 11:25:52 +0900

    Add test-svg project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { motion } from 'framer-motion'
+import Icon from './svg/icon.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div className="flex justify-center items-center min-h-screen bg-purple-900">
+        <Icon />
+    </motion.div>
   );
 }
 
