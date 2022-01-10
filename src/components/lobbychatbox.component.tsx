ef980941b6fffcaf9716ca5a395ac094217ca9b8
checker <trinhlehainam@protonmail.com>
2022-01-10 18:29:49 +0900

    Add lobbychatbox

---
+++ src/components/lobbychatbox.component.tsx
@@ -0,0 +1,30 @@
+import cx from 'classnames'
+
+const LobbyChatBox = () => {
+    return (
+        <div
+            className={cx(
+                'flex flex-col-reverse justify-start',
+                'absolute bottom-0 left-0 w-1/3 h-full text-yellow-custom',
+            )}
+        >
+            <input
+                type='text'
+                className='peer outline-none border-2 border-yellow-custom bg-gray-700/90 w-full text-lg'
+            />
+            <div
+                className={cx(
+                    'bottom-0 left-0 border-2 border-yellow-custom z-10',
+                    'bg-gray-700/90',
+                    'w-full h-0 opacity-0',
+                    'peer-focus:h-[400px] peer-focus:opacity-100',
+                    'transition-all ease-linear duration-[0.2s]',
+                    'overflow-auto scrollbar-hide'
+                )}
+            >
+            </div>
+        </div>
+    )
+};
+
+export default LobbyChatBox;
--- src/routes/lobby.route.tsx
+++ src/routes/lobby.route.tsx
@@ -1,4 +1,5 @@
 import cx from 'classnames'
+import LobbyChatBox from '../components/lobbychatbox.component';
 import PlayerCard from '../components/playercard.component';
 import Button from '../svg/lobby/button.svg';
 
@@ -21,28 +22,7 @@ const Lobby = () => {
                     <Button classname='h-auto' />
                     <Button classname='scale-75 h-auto' />
                 </div>
-                <div
-                    className={cx(
-                        'flex flex-col-reverse justify-start',
-                        'absolute bottom-0 left-0 w-1/3 h-full text-yellow-custom',
-                    )}
-                >
-                    <input
-                        type='text'
-                        className='peer outline-none border-2 border-yellow-custom bg-gray-700/90 w-full text-lg'
-                    />
-                    <div
-                        className={cx(
-                            'bottom-0 left-0 border-2 border-yellow-custom z-10',
-                            'bg-gray-700/90',
-                            'w-full h-0 opacity-0',
-                            'peer-focus:h-[400px] peer-focus:opacity-100',
-                            'transition-all ease-linear duration-[0.2s]',
-                            'overflow-auto scrollbar-hide'
-                        )}
-                    >
-                    </div>
-                </div>
+                <LobbyChatBox />
             </div>
         </div>
     )
